c48f504fd54d82c1a93380a8a4dc6fc0956ff5d7
ixil <ixil@debian.vb>
2020-02-24 21:02:41 +0000

    added basic directory handling

--- countreviews.sh
+++ countreviews.sh
@@ -7,3 +7,20 @@ then
 	grep -c "<Author>" "$1"
 
 fi
+
+if [ -d "$1" ]
+
+then
+	for entry in "$1"/*
+	do
+		grep -c "<Author>" "$entry"
+	done
+
+fi
+
+if [ ! -f "$1" ] && [ ! -d "$1" ]
+
+then
+	echo "put file or directory as argument"
+
+fi
